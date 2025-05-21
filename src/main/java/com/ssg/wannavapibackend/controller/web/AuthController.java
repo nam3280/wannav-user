9befff885906351e59cec26d3c5d684261a94bc9
nam3280 <nam3280@naver.com>
2025-05-21 23:22:25 +0900

    ♻️ recycle: 불필요한 코드 삭제

--- src/main/java/com/ssg/wannavapibackend/controller/web/AuthController.java
+++ src/main/java/com/ssg/wannavapibackend/controller/web/AuthController.java
@@ -1,14 +1,11 @@
 package com.ssg.wannavapibackend.controller.web;
 
-import com.ssg.wannavapibackend.security.util.JWTUtil;
 import com.ssg.wannavapibackend.service.LoginService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
@@ -18,17 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AuthController {
 
     private final LoginService kakaoService;
-    private final JWTUtil jwtUtil;
 
     @GetMapping("/login")
     public String login(Model model){
         model.addAttribute("location", kakaoService.getKakaoLogin());
         return "auth/login";
-    }
-
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response){
-        jwtUtil.removeCookie(response);
-        return "redirect:/";
     }
 }
