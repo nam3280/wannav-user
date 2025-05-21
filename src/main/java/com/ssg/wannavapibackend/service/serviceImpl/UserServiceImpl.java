cdece34d6253fdcad842181b6c574ad2df1c7f7b
nam3280 <nam3280@naver.com>
2025-05-21 21:34:31 +0900

    ♻️ recycle: UserServiceImpl 변경

--- src/main/java/com/ssg/wannavapibackend/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/ssg/wannavapibackend/service/serviceImpl/UserServiceImpl.java
@@ -1,47 +1,47 @@
 package com.ssg.wannavapibackend.service.serviceImpl;
 
 import com.ssg.wannavapibackend.domain.User;
-import com.ssg.wannavapibackend.dto.response.KakaoResponseDTO;
 import com.ssg.wannavapibackend.repository.UserRepository;
 import com.ssg.wannavapibackend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Log4j2
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
     @Transactional
-    public User saveUser(String nickName, String profileImage, String email) {
-        String chatbotCode = UUID.randomUUID().toString();
-        return User.builder().
-                    username(nickName).
-                    profile(profileImage).
-                    email(email).
-                    chatbotCode(chatbotCode).
-                    code(uniqueCode()).
-                    point(0L).
-                    consent(true).
-                    unregistered(false).
-                    createdAt(LocalDateTime.now()).
-                    build();
-    }
+    public User createUser(String nickName, String profile, String email) {
+        Optional<User> existingUser = userRepository.findUserByEmail(email);
 
-    @Override
-    @Transactional(readOnly = true)
-    public KakaoResponseDTO getUser(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid ID value: " + userId));
-        return new KakaoResponseDTO(user.getId(), user.getEmail(), user.getUsername());
+        if (existingUser.isPresent())
+            return existingUser.get();
+
+        String chatbotCode = UUID.randomUUID().toString();
+
+        User user = User.builder()
+                .id(null)
+                .username(nickName)
+                .profile(profile)
+                .email(email)
+                .chatbotCode(chatbotCode)
+                .code(uniqueCode())
+                .point(0L)
+                .consent(true)
+                .unregistered(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return userRepository.save(user);
     }
 
     private String uniqueCode() {
