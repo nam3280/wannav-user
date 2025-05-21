c6b45cf7283904b5b2e8b4fd4b3916d7b843d013
nam3280 <nam3280@naver.com>
2025-05-21 21:46:10 +0900

    ♻️ recycle: 코드 리팩토링

--- src/main/java/com/ssg/wannavapibackend/controller/api/EventRestController.java
+++ src/main/java/com/ssg/wannavapibackend/controller/api/EventRestController.java
@@ -1,12 +1,13 @@
 package com.ssg.wannavapibackend.controller.api;
 
 import com.ssg.wannavapibackend.dto.request.EventCouponRequestDTO;
+import com.ssg.wannavapibackend.security.principal.PrincipalDetails;
 import com.ssg.wannavapibackend.service.EventService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,11 @@ public class EventRestController {
     private final EventService eventService;
 
     @PostMapping("/coupon")
-    public ResponseEntity<String> coupon(@RequestBody EventCouponRequestDTO eventCouponRequestDTO){
+    public ResponseEntity<String> coupon(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
+            @RequestBody EventCouponRequestDTO eventCouponRequestDTO){
         try {
-            eventService.couponDistribution(eventCouponRequestDTO);
+            eventService.couponDistribution(Long.valueOf(principalDetails.getName()), eventCouponRequestDTO);
         } catch (RuntimeException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
