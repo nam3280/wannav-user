c80104a6e360d9351017dd1f3ed68915ac0a14ad
nam3280 <nam3280@naver.com>
2025-05-21 21:46:40 +0900

    ♻️ recycle: 코드 리팩토링

--- src/main/java/com/ssg/wannavapibackend/controller/web/AuthController.java
+++ src/main/java/com/ssg/wannavapibackend/controller/web/AuthController.java
@@ -1,20 +1,15 @@
 package com.ssg.wannavapibackend.controller.web;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ssg.wannavapibackend.dto.response.KakaoResponseDTO;
 import com.ssg.wannavapibackend.security.util.JWTUtil;
-import com.ssg.wannavapibackend.service.KakaoService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
+import com.ssg.wannavapibackend.service.LoginService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Log4j2
 @Controller
@@ -22,7 +17,7 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final KakaoService kakaoService;
+    private final LoginService kakaoService;
     private final JWTUtil jwtUtil;
 
     @GetMapping("/login")
@@ -31,36 +26,7 @@ public class AuthController {
         return "auth/login";
     }
 
-    @GetMapping("/callback")
-    public String callback(HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException {
-        KakaoResponseDTO kakaoResponseDTO = kakaoService.getKakaoInfo(request.getParameter("code"));
-
-        Long userId = kakaoResponseDTO.getId();
-        Map<String, Object> dataMap = kakaoResponseDTO.getDataMap();
-
-        String accessToken = jwtUtil.createToken(dataMap, 60);
-        String refreshToken = jwtUtil.createToken(Map.of("mid", userId), 60 * 3);
-
-        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
-        accessTokenCookie.setMaxAge(60);
-        accessTokenCookie.setHttpOnly(true);
-//        accessTokenCookie.setSecure(true); // https 가능 (도메인)
-        accessTokenCookie.setPath("/");
-
-        response.addCookie(accessTokenCookie);
-
-        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-        refreshTokenCookie.setMaxAge(60 * 3);
-        refreshTokenCookie.setHttpOnly(true);
-//        refreshTokenCookie.setSecure(true); // https 가능 (도메인)
-        refreshTokenCookie.setPath("/");
-
-        response.addCookie(refreshTokenCookie);
-
-        return "redirect:/";
-    }
-
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String logout(HttpServletResponse response){
         jwtUtil.removeCookie(response);
         return "redirect:/";
