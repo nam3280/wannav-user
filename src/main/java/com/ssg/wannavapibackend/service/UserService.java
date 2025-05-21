b3fab5125c2109736cafe604050e5671cc5b6304
nam3280 <nam3280@naver.com>
2025-05-21 21:34:15 +0900

    ♻️ recycle: UserService 변경

--- src/main/java/com/ssg/wannavapibackend/service/UserService.java
+++ src/main/java/com/ssg/wannavapibackend/service/UserService.java
@@ -1,10 +1,7 @@
 package com.ssg.wannavapibackend.service;
 
 import com.ssg.wannavapibackend.domain.User;
-import com.ssg.wannavapibackend.dto.response.KakaoResponseDTO;
 
 public interface UserService {
-    User saveUser(String nickName, String profileImage, String email);
-
-    KakaoResponseDTO getUser(Long userId);
+    User createUser(String nickName, String profile, String email);
 }
