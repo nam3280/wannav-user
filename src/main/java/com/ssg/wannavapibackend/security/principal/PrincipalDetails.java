019c9c95d036338daaac7be99c0178a77281b632
nam3280 <nam3280@naver.com>
2025-05-21 22:42:45 +0900

    ♻️ recycle: 불필요한 코드 제거

--- src/main/java/com/ssg/wannavapibackend/security/principal/PrincipalDetails.java
+++ src/main/java/com/ssg/wannavapibackend/security/principal/PrincipalDetails.java
@@ -2,14 +2,13 @@ package com.ssg.wannavapibackend.security.principal;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
-public class PrincipalDetails implements OAuth2User, UserDetails {
+public class PrincipalDetails implements OAuth2User {
 
     private final String id;
     private final Map<String, Object> attributes;
@@ -32,35 +31,5 @@ public class PrincipalDetails implements OAuth2User, UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return id;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
