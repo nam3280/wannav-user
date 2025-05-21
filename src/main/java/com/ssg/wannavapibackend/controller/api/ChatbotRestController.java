1c7f29e2999b48df3dbcea69094e7f28a01cb646
nam3280 <nam3280@naver.com>
2025-05-21 21:46:17 +0900

    ♻️ recycle: 코드 리팩토링

--- src/main/java/com/ssg/wannavapibackend/controller/api/ChatbotRestController.java
+++ src/main/java/com/ssg/wannavapibackend/controller/api/ChatbotRestController.java
@@ -1,9 +1,10 @@
 package com.ssg.wannavapibackend.controller.api;
 
-import com.ssg.wannavapibackend.security.util.JWTUtil;
+import com.ssg.wannavapibackend.security.principal.PrincipalDetails;
 import com.ssg.wannavapibackend.service.ChatbotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatbotRestController {
 
     private final ChatbotService chatbotService;
-    private final JWTUtil jwtUtil;
 
     @PostMapping("/send-message")
-    public String sendMessage(@RequestBody String requestMessage) {
-        return chatbotService.sendMessage(jwtUtil.getUserId(), requestMessage);
+    public String sendMessage(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
+            @RequestBody String requestMessage) {
+        return chatbotService.sendMessage(Long.parseLong(principalDetails.getName()), requestMessage);
     }
 }
