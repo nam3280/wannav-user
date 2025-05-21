85b6d9c0d2f6714338f8ff3e85507e11adbce723
nam3280 <nam3280@naver.com>
2025-05-21 21:47:00 +0900

    ♻️ recycle: 코드 리팩토링

--- src/main/java/com/ssg/wannavapibackend/config/SecurityConfig.java
+++ src/main/java/com/ssg/wannavapibackend/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.ssg.wannavapibackend.security.handler.OAuthLogoutSuccessHandler;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
